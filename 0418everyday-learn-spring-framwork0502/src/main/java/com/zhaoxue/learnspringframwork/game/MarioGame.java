4aff5eaa9aa5864f4493eef6d6df1967423c7ab9
zhaoxue <fromzhaoxue@gmail.com>
2024-04-19 20:31:54 +0900

    @component @primary

--- 0418everyday-learn-spring-framwork0502/src/main/java/com/zhaoxue/learnspringframwork/game/MarioGame.java
+++ 0418everyday-learn-spring-framwork0502/src/main/java/com/zhaoxue/learnspringframwork/game/MarioGame.java
@@ -1,21 +1,30 @@
 package com.zhaoxue.learnspringframwork.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import com.zhaoxue.learnspringframwork.GamingConsole;
 
+@Component
+@Primary
 public class MarioGame implements GamingConsole {
 
+	@Override
 	public void up() {
 		System.out.println("Jump");
 	}
 
+	@Override
 	public void down() {
 		System.out.println("Go into a hole");
 	}
 
+	@Override
 	public void left() {
 		System.out.println("Go back");
 	}
 
+	@Override
 	public void right() {
 		System.out.println("Accelerate");
 	}
